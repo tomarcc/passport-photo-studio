cd0ce72066545126396a2cc7a5e63557be49d845
tomarcc <tomarchayan@gmail.com>
2025-11-08 23:26:59 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/madhabam/passportstudio/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.madhabam.passportstudio
+
+import android.Manifest
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    // NOTE: This is a skeleton. Full CameraX + ML Kit capture/segmentation code to be added.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
